9529524806acf76141fa02651e84d237ffece68f
Sean Stephens <seanstephens29@gmail.com>
2016-11-15 16:59:32 -0700

    2.0.6

---
+++ addon/index.js
@@ -0,0 +1,24 @@
+/**
+ * defualt busy-utils
+ *
+ */
+import Assert from './assert';
+import Browser from './browser';
+import Crypto from './crypto';
+import Time from './time';
+import UUID from './uuid';
+import loc from './loc';
+import Locale from './locale';
+import LocalStorage from './local-storage';
+
+
+export {
+	Assert,
+	Browser,
+	Crypto,
+	loc,
+	LocalStorage,
+	Locale,
+	Time,
+	UUID
+};
