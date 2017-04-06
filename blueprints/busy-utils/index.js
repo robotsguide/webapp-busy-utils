44063685b563d60cc69d298add2729dbd561eabb
Sean Stephens <seanstephens29@gmail.com>
2017-04-06 15:44:10 -0600

    fixed blueprint again

--- blueprints/busy-utils/index.js
+++ blueprints/busy-utils/index.js
@@ -3,7 +3,7 @@ module.exports = {
 	normalizeEntityName() {},
 
 	afterInstall() {
-		return this.addAddonsToProject(["ember-moment@7.3.0", "ember-uuid@1.0.0"]).then(() => {
+		return this.addAddonsToProject({packages: ["ember-moment@7.3.0", "ember-uuid@1.0.0"]}).then(() => {
 			return this.addPackageToProject("crypto-js@3.1.6");
 		});
 	}
