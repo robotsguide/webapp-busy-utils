f5b98926dadbbec2b50cfbe5b6d4344866708871
Sean Stephens <seanstephens29@gmail.com>
2017-04-06 17:22:06 -0600

    fix(blueprint): updated addon includes

--- blueprints/busy-utils/index.js
+++ blueprints/busy-utils/index.js
@@ -3,8 +3,8 @@ module.exports = {
 	normalizeEntityName() {},
 
 	afterInstall() {
-		return this.addAddonToProject("ember-moment", "~7.3.0").then(() => {
-			return this.addAddonToProject("ember-uuid", "~1.0.0");
+		return this.addAddonToProject("ember-moment", "7.3.0").then(() => {
+			return this.addAddonToProject("ember-uuid", "1.0.0");
 		});
 	}
 };
