90ceed8e6e7e4fb8985d98db461f620e02b54b9c
Sean Stephens <seanstephens29@gmail.com>
2017-04-06 15:37:40 -0600

    fix blueprint

--- blueprints/busy-utils/index.js
+++ blueprints/busy-utils/index.js
@@ -1,15 +1,8 @@
 /* eslint-env node */
 module.exports = {
-	description: '',
+	normalizeEntityName() {},
 
-	// locals: function(options) {
-	//   // Return custom template variables here.
-	//   return {
-	//     foo: options.entity.options.foo
-	//   };
-	// }
-
-	afterInstall: function() {
+	afterInstall() {
 		return this.addAddonsToProject({"ember-moment": "7.3.0", "ember-uuid": "1.0.0"}).then(() => {
 			return this.addPackageToProject("crypto-js@3.1.6");
 		});
