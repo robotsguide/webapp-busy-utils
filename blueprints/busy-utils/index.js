17afff3c865c39601baf50e999e81224512dd5f2
Sean Stephens <seanstephens29@gmail.com>
2017-04-06 17:12:25 -0600

    another blueprint test

--- blueprints/busy-utils/index.js
+++ blueprints/busy-utils/index.js
@@ -3,11 +3,8 @@ module.exports = {
 	normalizeEntityName() {},
 
 	afterInstall() {
-		return this.addAddonsToProject({
-			packages: [
-				"ember-moment@~7.3.0",
-				"ember-uuid@~1.0.0"
-			]
+		return this.addAddonToProject("ember-moment", "~7.3.0").then(() => {
+			return this.addAddonToProject("ember-uuid", "~1.0.0");
 		});
 	}
 };
