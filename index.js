3d07ddc5cc1004a54bc0f0fe030cec2aa73f8e6a
Sean Stephens <seanstephens29@gmail.com>
2017-10-30 17:12:15 -0600

    upgraded to ember-2.16.2

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
 /* eslint-env node */
 'use strict';
-var commands = require('./lib/commands');
+
+const commands = require('./lib/commands');
 
 module.exports = {
   name: 'busy-utils',
-
-  included(app) {
-    this._super.included(app);
-
-		// see: https://github.com/ember-cli/ember-cli/issues/3718
-		while (typeof app.import !== 'function' && app.app) {
-			app = app.app;
-		}
-
-		this.app = app;
-  },
 
 	includedCommands() {
 		return commands;
